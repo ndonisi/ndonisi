12ef4f4e517ffd9bf6c55221a44485d73db5f966
ndonisi <72038603+ndonisi@users.noreply.github.com>
2021-02-14 20:13:11 -0800

    added confetti

---
+++ Project1/js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+    var flakes = '',
+        randomColor;
+
+    for(var i = 0, len = 400; i < len; i++) {
+        randomColor = Math.floor(Math.random()*16777215).toString(16);
+        flakes += '<div class="ball" style="background: #'+randomColor;
+        flakes += '; animation-duration: '+(Math.random() * 9 + 2)+'s; animation-delay: ';
+        flakes += (Math.random() * 2 + 0)+'s;"></div>';
+    }
+    document.getElementById('confetti').innerHTML = flakes;
+});
+
+    
