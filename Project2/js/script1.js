818811525efe2a236b7999c96425ef60d0218d24
ndonisi <72038603+ndonisi@users.noreply.github.com>
2021-03-14 14:22:17 -0700

    form

--- Project2/js/script1.js
+++ Project2/js/script1.js
@@ -29,10 +29,3 @@ $(window).on('mousemove click', function(e) {
 });
 
 moveBackground();
-
-$('.card').on('click', function() {
-  $('.card').removeClass('active');
-  $(this).addClass('active');
-  $('.form').stop().slideUp();
-  $('.form').delay(300).slideDown();
-});
