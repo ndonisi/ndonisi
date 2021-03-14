de516f44bb83518b28bb4e750cef6081f3a64331
ndonisi <72038603+ndonisi@users.noreply.github.com>
2021-03-14 09:27:16 -0700

    payment form

--- Project2/js/script1.js
+++ Project2/js/script1.js
@@ -29,3 +29,10 @@ $(window).on('mousemove click', function(e) {
 });
 
 moveBackground();
+
+$('.card').on('click', function() {
+  $('.card').removeClass('active');
+  $(this).addClass('active');
+  $('.form').stop().slideUp();
+  $('.form').delay(300).slideDown();
+});
